7948e362fa0a0c98b51d1278a8cfa1cf06e05d7c
Pedro Catana <pedrocatana@sapo.pt>
2018-04-05 19:27:37 +0100

    added Game class

--- core/src/org/academiadecodigo/hackathon/foxtrot/Game.java
+++ core/src/org/academiadecodigo/hackathon/foxtrot/Game.java
@@ -3,31 +3,47 @@ package org.academiadecodigo.hackathon.foxtrot;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+
+    private GameMap gameMap;
+
+    @Override
+    public void create() {
+
+        batch = new SpriteBatch();
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+        camera.update();
+
+        gameMap = new TiledGameMap();
+    }
+
+    @Override
+    public void render() {
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        gameMap.update(Gdx.graphics.getDeltaTime());
+        gameMap.render(camera, batch);
+    }
+
+    @Override
+    public void dispose() {
+
+        batch.dispose();
+        gameMap.dispose();
+    }
 }
