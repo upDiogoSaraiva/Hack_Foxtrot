6c71383dfa9fe89771d2361e6f8168f192310dd2
Pedro Catana <pedrocatana@sapo.pt>
2018-04-05 18:48:22 +0100

    added .gitignore and libGDX project

---
+++ desktop/src/org/academiadecodigo/hackathon/foxtrot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.hackathon.foxtrot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.hackathon.foxtrot.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
