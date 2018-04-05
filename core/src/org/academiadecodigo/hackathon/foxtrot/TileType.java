1eadc5f845a66ef017fb2973c6304e5e1b43266a
upDiogoSaraiva <diogo.saraiva@hotmail.com>
2018-04-05 21:17:10 +0100

    map with new size

--- core/src/org/academiadecodigo/hackathon/foxtrot/TileType.java
+++ core/src/org/academiadecodigo/hackathon/foxtrot/TileType.java
@@ -6,11 +6,11 @@ import java.util.Map;
 public enum TileType {
 
     GRASS (1, true, "Grass"),
-    DIRT (2, false, "Dirt"),
+    DIRT (2, true, "Dirt"),
     SKY (3, false, "Sky"),
     LAVA (4, false, "Lava"),
     CLOUD (5, true, "Cloud"),
-    STONE (6, false, "Stone");
+    STONE (6, true, "Stone");
 
     public static final int TILE_SIZE = 32;
 
