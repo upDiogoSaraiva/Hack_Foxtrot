2400617c4743b0ece89fbeae4224cd66882c64e2
tiago.fareleiro <tiago_f93@hotmail.com>
2018-04-05 20:18:01 +0100

    class player

--- core/src/org/academiadecodigo/hackathon/foxtrot/entities/Player.java
+++ core/src/org/academiadecodigo/hackathon/foxtrot/entities/Player.java
@@ -1,4 +1,47 @@
 package org.academiadecodigo.hackathon.foxtrot.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.academiadecodigo.hackathon.foxtrot.Game;
+
 public class Player extends Entity {
+
+    private static final int SPEED = 80;
+    private static final int JUMP_VELOCITY = 5;
+
+    private Texture image;
+
+    public Player(float x, float y, Game game) {
+        super(x, y, EntityType.PLAYER, map);
+        image = new Texture("player.png");
+    }
+
+    @Override
+    public void update(float deltaTime, float gravity) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && grounded) {
+            this.velocityY += JUMP_VELOCITY * getWeight();
+
+        } else if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !grounded && velocityY > 0) {
+            this.velocityY += JUMP_VELOCITY * getWeight() * deltaTime;
+        }
+
+        super.update(deltaTime, gravity);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            moveX(-SPEED * deltaTime);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            moveX(SPEED * deltaTime);
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        public void render (SpriteBatch batch){
+            batch.draw(image, pos.x, pos.y, getWidth(), getHeight());
+        }
+    }
 }
