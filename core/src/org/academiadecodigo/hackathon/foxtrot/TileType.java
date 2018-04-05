77e97286b53dd435f118e3f1b6fd5f27fa5e0c51
Pedro Catana <pedrocatana@sapo.pt>
2018-04-05 19:21:47 +0100

    added TileType

--- core/src/org/academiadecodigo/hackathon/foxtrot/TileType.java
+++ core/src/org/academiadecodigo/hackathon/foxtrot/TileType.java
@@ -1,4 +1,63 @@
 package org.academiadecodigo.hackathon.foxtrot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TileType {
+
+    GRASS (1, true, "Grass"),
+    DIRT (2, true, "Dirt"),
+    SKY (3, false, "Sky"),
+    LAVA (4, false, "Lava"),
+    CLOUD (5, true, "Cloud"),
+    STONE (6, true, "Stone");
+
+    public static final int TILE_SIZE = 32;
+
+    private int id;
+    private boolean collidable;
+    private String name;
+    private float damage;
+    private static Map<Integer, TileType> tileMap;
+
+    TileType (int id, boolean collidable, String name) {
+        this(id, collidable, name, 0);
+    }
+
+    TileType (int id, boolean collidable, String name, float damage) {
+
+        this.id = id;
+        this.collidable = collidable;
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isCollidable() {
+        return collidable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    static {
+
+        tileMap = new HashMap<Integer, TileType>();
+
+        for (TileType tileType: TileType.values()) {
+            tileMap.put(tileType.getId(), tileType);
+        }
+    }
+
+    public static TileType getTileTypeById (int id) {
+        return tileMap.get(id);
+    }
 }
