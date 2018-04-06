7f9bd73fe8f14a07b5b5f3527d90831a8c2c93d1
Pedro Catana <pedrocatana@sapo.pt>
2018-04-06 01:29:39 +0100

    added class coffin, update map.tmx with layer 3

---
+++ core/src/org/academiadecodigo/hackathon/foxtrot/entities/Coffin.java
@@ -0,0 +1,16 @@
+package org.academiadecodigo.hackathon.foxtrot.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.academiadecodigo.hackathon.foxtrot.GameMap;
+
+public class Coffin extends Entity {
+
+    public Coffin(float x, float y, EntityType type, GameMap map) {
+        super(x, y, type, map);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
--- core/src/org/academiadecodigo/hackathon/foxtrot/entities/EntityType.java
+++ core/src/org/academiadecodigo/hackathon/foxtrot/entities/EntityType.java
@@ -2,7 +2,8 @@ package org.academiadecodigo.hackathon.foxtrot.entities;
 
 public enum EntityType {
 
-    PLAYER("player", 14, 32, 40);
+    PLAYER("player", 14, 32, 40),
+    COFFIN("coffin", 14, 32, 40);
 
     private String id;
     private int width;
